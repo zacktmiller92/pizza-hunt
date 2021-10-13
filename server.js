3a85dcd7993a2dd882ce795b2c21e91d422443e5
zacktmiller92 <zacktmiller92@gmail.com>
2021-10-13 19:20:45 -0400

    downgraded dependencies

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.use(express.static('public'));
 app.use(require('./routes'));
 
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/pizza-hunt', {
-    // useFindAndModify: false,
+    useFindAndModify: false,
     useNewUrlParser: true,
     useUnifiedTopology: true
 });
